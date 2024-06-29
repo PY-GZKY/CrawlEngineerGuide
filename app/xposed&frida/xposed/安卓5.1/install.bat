9b977d2e2959793a58c36c92ce0c07c03f2b0b0c
PY-GZKY <341796767@qq.com>
2024-06-30 02:34:59 +0800

    重构文档

--- app/xposed&frida/xposed/安卓5.1/install.bat
+++
@@ -1,5 +0,0 @@
-adb root
-adb remount
-adb push ./xposed /system
-adb shell su -c "cd /system/;sh memu-script.sh"
-pause
--- app/xposed&frida/xposed/安卓7.1/install.bat
+++
@@ -1,5 +0,0 @@
-adb root
-adb remount
-adb push C:/Users/pai/Downloads/xposed/7/xposed /system
-adb shell su -c "cd /system/; sh ./memu-script.sh"
-pause
